c67c0ff0cc2b660b07c5fa823f03df4db0ddf354
TheT87 <ytacc87@gmail.com>
2019-05-01 13:52:17 +0200

    W2u3 added

--- Woche1/src/Main.hs
+++ Woche1/src/Main.hs
@@ -1,16 +1,14 @@
 
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
+fib_output n = fib !! n
+
 factorial :: Integer -> Integer
 factorial 0 = 1
 factorial 1 = 1
 factorial n = n*factorial (n-1)
 
-fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
-fib_output n = fib !! n
-
---------#
 
 sumFac :: Integer -> Integer -> Integer
-
 sumFac n m
   | m >= n = factorial m + sumFac n (m-1)
   | otherwise = 0
@@ -21,7 +19,6 @@ sumFacs n m =
   then factorial m + sumFacs n (m-1)
   else 0
 
---------#
 
 rem1:: Int -> [Int] -> [Int]
 rem1 _ []=[]
@@ -29,7 +26,7 @@ rem1 n (x:xs)
   | n /= x = x : rem1 n xs
   | otherwise = rem1 n xs
 
--------#
+
 
 isOrd:: [Int] -> Bool
 isOrd [] = True
